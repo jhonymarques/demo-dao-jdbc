8fe2d924fefacea631c2cad7abae62b3d7e0acd3
Juca-Home <jonathamarques_2006@hotmail.com>
2019-11-15 12:46:53 -0300

    DepartmentDao and SellerDao Interfaces

---
+++ src/model/dao/DapartmentDao.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DapartmentDao {
+
+    void insert (Department obj);
+    void  update (Department obj);
+    void  deleteById (Integer id);
+    Department findById (Integer id);
+    List<Department> findAll();
+
+
+    }
+
+
+
+
+
