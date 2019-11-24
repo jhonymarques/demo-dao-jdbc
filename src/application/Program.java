a8ee6de262d9e1bd222e2067027b6b05757b73d4
Juca-Home <jonathamarques_2006@hotmail.com>
2019-11-24 19:33:10 -0300

    FindByID Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,14 +12,16 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department (1, " Books");
+       /* Department obj = new Department (1, " Books");
         System.out.println(obj);
 
 
         Seller seller = new Seller(23, "Juca", "juca@juca.com" , new Date(), 6000.0, obj);
-        System.out.println(seller);
+        System.out.println(seller);*/
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
 
     }
